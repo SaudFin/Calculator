6a47dfd1a2a7e4dbf3d4b354320938c060c9c527
SaudFin <developer.saud@gmail.com>
2020-08-30 05:20:16 +0300

    basic math operators functions are added

---
+++ main.js
@@ -0,0 +1,19 @@
+// Addition
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+// subtraction
+const subtract = (num1, num2) => {
+  return num1 - num2;
+};
+
+// multiplication
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
+
+// division
+const divide = (num1, num2) => {
+  return num1 / num2;
+};
