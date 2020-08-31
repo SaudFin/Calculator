398e7838ca468d0eef2a61871fab777adab8f366
SaudFin <developer.saud@gmail.com>
2020-08-31 05:46:09 +0300

    numbers are displayed on the result screen when clicked

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+let nums = document.querySelectorAll(".num");
+let result = document.getElementById("result");
+nums = Array.from(nums);
+
+// to display numbers on the result screen
+const resultScreen = numElement => {
+  //   console.log(numElement, "herre");
+  numElement.addEventListener("click", () => {
+    result.innerHTML = numElement.textContent;
+  });
+};
+
+// loop through nums elements and activiate the event listener
+nums.forEach(resultScreen);
+
 // Addition
 const add = (num1, num2) => {
   return num1 + num2;
