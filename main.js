c3c192b01bb5e1d957564f69e5513c57a589583f
SaudFin <developer.saud@gmail.com>
2020-09-03 21:17:32 +0300

    few bugs are fixed, and number zero is added

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let nums = document.querySelectorAll(".num");
 let result = document.getElementById("result");
 let operaters = document.querySelectorAll(".operator");
 let equalizer = document.getElementById("equal");
+let operation = false;
+let clearButton = document.getElementById("clear");
 // to display result in the screen results
 let total = 0;
 // to keep caculating all totals
@@ -14,7 +16,12 @@ let action;
 // to display numbers on the result screen
 const resultScreen = numElement => {
   numElement.addEventListener("click", () => {
-    result.innerHTML = numElement.textContent;
+    if (operation === false && result.innerHTM != 0) {
+      result.innerHTML = numElement.textContent;
+      operation = true;
+    } else {
+      result.innerHTML = result.innerHTML + numElement.textContent;
+    }
   });
 };
 // to activate the math operation
@@ -22,14 +29,29 @@ const operaterAction = operateElement => {
   operateElement.addEventListener("click", () => {
     firstNum = result.innerHTML;
     action = operateElement.id;
+    operation = false;
   });
 };
+
+// to get the result of the math operation
 equalizer.addEventListener("click", () => {
-  let secondNum = result.innerHTML;
-  firstNum = parseInt(firstNum);
-  secondNum = parseInt(secondNum);
-  operate(firstNum, action, secondNum);
+  if (operation === true) {
+    let secondNum = result.innerHTML;
+    firstNum = parseInt(firstNum);
+    secondNum = parseInt(secondNum);
+
+    operate(firstNum, action, secondNum);
+  }
 });
+
+// to clear the screen results and the total
+const clearResult = () => {
+  result.innerHTML = 0;
+  operation = false;
+  total = 0;
+};
+
+clearButton.addEventListener("click", clearResult);
 // loop through nums elements and activiate the event listener
 nums.forEach(resultScreen);
 // loop through operaters elements and activiate the event listener
@@ -41,6 +63,7 @@ const add = (num1, num2) => {
 
 // subtraction
 const subtract = (num1, num2) => {
+  console.log(num1, num2);
   total = num1 - num2;
 };
 
@@ -56,7 +79,7 @@ const divide = (num1, num2) => {
 
 // CALCULATOR
 const operate = (num1, operater, num2) => {
-  console.log(operater, num1, num2);
+  // console.log(operater, num1, num2);
   switch (operater) {
     case "multiply":
       multiply(num1, num2);
@@ -71,7 +94,6 @@ const operate = (num1, operater, num2) => {
       subtract(num1, num2);
       break;
   }
-
   result.innerHTML = total;
   // lastResult = result.innerHTML;
 };
